6f1c8ffc0467a317b738ae3d20bca15a3a7132e5
Karan <karanchandravanshi217@gmail.com>
2023-02-25 21:47:05 +0530

    Completed

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const accordion = document.getElementsByClassName('contentBx');
+const accordion = document.getElementsByClassName("contentBx");
 
-for(i=0; i<accordion.length; i++){
-    accordion[i].addEventListener('click', function(){
-        this.classList.toggle('active')
-    })
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    for (j = 0; j < accordion.length; j++) {
+      
+      if (accordion[j] !== this && accordion[j].classList.contains("active")) {
+        accordion[j].classList.remove("active");
+      }
+    }
+  });
+}
+
+function showDiv(divId) {
+  const div1 = document.getElementById("Div1");
+  const div2 = document.getElementById("Div2");
+  const div3 = document.getElementById("Div3");
+  var div = document.getElementById(divId);
+
+  if (divId == "Div1") {
+  div1.style.display = "block";
+  div2.style.display = "none";
+  div3.style.display = "none";
+} else {
+  div1.style.display = "none";
+  div2.style.display = "none";
+  div3.style.display = "none";
+  div.style.display = "block";
+}
 }
 
 
-  function showDiv(divId) {
-    // Hide all div elements
-    // var divs = document.getElementsById("div");
-    // for (var i = 0; i < divs.length; i++) {
-    //   divs[i].style.display = "none";
-    // }
-    
-    // Show the selected div element
-    var div = document.getElementById(divId);
-    div.style.display = "block";
-    
-  }
-  
+
